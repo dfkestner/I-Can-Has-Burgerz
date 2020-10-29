ec77a3b87359ae288d45e1f1d780f380b6589f99
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-29 18:50:08 -0400

    Updated burgers.js with ajax calls for devouring, adding, and deleting burgers

---
+++ public/burgers.js
@@ -0,0 +1,47 @@
+$(function() {
+    $(".devour").on("click", function(event) {
+        let id = $(this).data("id");
+        let devour = $(this).data("devoured");
+
+        let devouredStatus = {
+            devoured: devour
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredStatus
+        }).then(function() {
+            console.log(data);
+            location.reload();
+        });
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        let addBurger = {
+            name: $("#burgerz").val().trim(),
+            devoured: $("[burger_name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: addBurger
+        }).then(function() {
+                console.log(data);
+                location.reload();
+            }
+        );
+    });
+
+    $(".byeburgerz").on("click", function(event) {
+        let id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(function() {
+            console.log(id);
+            location.reload();
+        });
+    });
+});
