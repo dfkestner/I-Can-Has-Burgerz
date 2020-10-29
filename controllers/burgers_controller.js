f572a354bd3d241e7cd3e19be19afed3c38d2fa1
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-29 17:51:38 -0400

    Added delete route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,12 +21,24 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("api/burgers/:id", function(req, res) {
     let condition = "id = " + req.params.id;
-    console.log("condition", condition)
 
     burger.updateOne({devoured: req.body.devoured}, condition, function(result) {
         if (result.changedRows === 0) {
             return res.status(404).end();
         }
+        else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/cats/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.deleteOne(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } 
         else {
             res.status(200).end();
         }
